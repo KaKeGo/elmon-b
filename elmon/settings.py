cd617a55bceb63d81f5b41b182075fbd339790ac
KaKeGo <funfcia@hotmail.com>
2023-04-30 23:04:09 +0200

    Custom user

--- elmon/settings.py
+++ elmon/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    'users',
+    
+    # Third apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
